b517c60087e8cd5e2f9ba017a0cfe4905b47e0f5
NonstopViet <nonstopviet2017@gmail.com>
2017-02-25 00:34:31 +0700

    Add files via upload

---
+++ ngoquangtruong.js
@@ -0,0 +1,20 @@
+<div style='line-height: 24px;'>
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.startemplatevn.net' rel='nofollow' target='_blank' title='Template VN'>Template VN</a> / 
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.nonstopviet.com' rel='nofollow' target='_blank' title='NonstopViet.Com'>Nonstop Việt</a> / 
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.giangna.com' rel='nofollow' target='_blank' title='Giang Blogger'>Giang Blogger</a> / 
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.vanthangit.com' rel='nofollow' target='_blank' title='Văn Thắng Blog'>Văn Thắng Blog</a> / 
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.startuanit.net' rel='nofollow' target='_blank' title='Star Tuấn Blog'>Star Tuấn Blog</a> / 
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.lichblog.com' rel='nofollow' target='_blank' title='Học Photoshop'>Học Photoshop</a> / 
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.bacsiwindows.com' rel='nofollow' target='_blank' title='Blog Thủ Thuật CN'>Blog Thủ Thuật CN</a> / 
+<a href='http://www.blogchiasethuthuat.com/p/go.html?t=http://www.itidea.org' rel='nofollow' target='_blank' title='Itidea - Thủ Thuật Wordpress'>Itidea - Thủ Thuật Wordpress</a> / 
+<a href="http://www.blogchiasethuthuat.com/p/go.html?t=https://www.vutienit.com/" target="blank" title='Thủ Thuật Windows'>Thủ Thuật Windows</a> / 
+<a href="http://www.blogchiasethuthuat.com/p/go.html?t=http://www.omytips.net" target="blank" title='Omytips.net'>Omytips.net</a> / 
+<a href="http://www.blogchiasethuthuat.com/p/go.html?t=http://caonguyenit.com" target="blank" title='CaoNguyenIT.Com'>CaoNguyenIT.Com</a> / 
+<a href="http://www.blogchiasethuthuat.com/p/go.html?t=http://www.vutienanh.pro" target="blank" title='Vũ Tiến Anh Blog'>Vũ Tiến Anh Blog</a>
+</div>
+
+
+<!-- Banner Fshare -->
+<div style='text-align:center;width:100%;margin: 20px 0 0'>
+<a href="https://www.fshare.vn/partnerlink?key=f1startruongitnet" target='_blank'><img alt="Fshare" title="Fshare" src="https://www.fshare.vn/media/banner_Fshare/startruongit.net_300x250.gif"/></a>
+</div>
